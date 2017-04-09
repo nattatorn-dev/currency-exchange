cf5a2eb29d5018c3da474f644b74565691e73088
nattatorn.dev <nattatorn.dev@gmail.com>
2017-04-09 22:37:01 +0700

    :tada: first commit

---
+++ .storybook/config.js
@@ -0,0 +1,12 @@
+import { configure, setAddon } from '@kadira/storybook'
+import infoAddon from '@kadira/react-storybook-addon-info'
+
+const req = require.context( '../src', true, /.stories.js$/ )
+
+function loadStories () {
+  req.keys().forEach( filename => req( filename ) )
+}
+
+setAddon( infoAddon )
+
+configure( loadStories, module )
---
+++ .storybook/webpack.config.js
@@ -0,0 +1,43 @@
+// you can use this file to add your custom webpack plugins, loaders and anything you like.
+// This is just the basic way to add addional webpack configurations.
+// For more information refer the docs: https://getstorybook.io/docs/configurations/custom-webpack-config
+
+// IMPORTANT
+// When you add this file, we won't add the default configurations which is similar
+// to "React Create App". This only has babel loader to load JavaScript.
+
+module.exports = {
+  plugins: [
+    // your custom plugins
+  ],
+  module: {
+    loaders: [
+      { test: /\.json$/, loader: 'json-loader' },
+      {
+        test: /\.less$/,
+        loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!postcss-loader!less?outputStyle=expanded&sourceMap',
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!postcss-loader!sass?outputStyle=expanded&sourceMap',
+      },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream',
+      },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml',
+      },
+    ],
+  },
+}
