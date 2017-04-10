69e17fc8e4b66470ea5bf6fd0b5e5c4ec0a3ff0c
nattatorn.dev <nattatorn.dev@gmail.com>
2017-04-10 21:58:14 +0700

    💄 increase space for import path

--- src/shared/BankImageCircle.js
+++ src/shared/BankImageCircle.js
@@ -1,5 +1,5 @@
-import styled  from 'styled-components'
-import { url } from 'helpers'
+import styled    from 'styled-components'
+import { url }   from 'helpers'
 
 import BankImage from 'shared/BankImage'
 
